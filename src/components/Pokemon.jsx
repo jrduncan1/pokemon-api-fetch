90577e06ac7cc0daa9643af4b735755e876d53b0
jrduncan1 <dunc5201@gmail.com>
2022-01-19 19:52:05 -0700

    pokemon api fetch method complete

---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const Pokemon = (props) => {
+    const [pokemon, setPokemon] = useState([]);
+    const [clickStatus, setClickStatus] = useState(false);
+
+    const fetchPokemon = () => {
+        setClickStatus(true);
+        fetch("https://pokeapi.co/api/v2/pokemon")
+            .then(response => response.json())
+            .then(response => setPokemon(response.results))
+    }
+
+    return(
+        <div>
+            <button onClick={fetchPokemon}>Fetch Pokemon!</button>
+            {pokemon.map((pokemon, idx) => {
+                return(
+                    <div key={idx}>{pokemon.name}</div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Pokemon;
